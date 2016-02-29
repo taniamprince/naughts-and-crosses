16da675008ae2622653a061cfd12f60099a8ef01
taniamprince <taniamprince@gmail.com>
2016-02-29 12:11:44 -0500

    cleaned up test cases

--- rspec/board_rspec.rb
+++ rspec/board_rspec.rb
@@ -13,9 +13,7 @@ RSpec.describe Board, "#initialize" do
 end
 
 # Board for testing
-state = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-turn = "CPU"
-board = Board.new state, turn
+board = Board.new [1, 2, 3, 4, 5, 6, 7, 8, 9], "CPU"
 
 RSpec.describe Board, "#new_board" do
 	context "with valid board object as argument" do
