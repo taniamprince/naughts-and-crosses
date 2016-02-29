1e23bc0c0c89d6708a4047f97a8722d44b5f8f90
taniamprince <taniamprince@gmail.com>
2016-02-29 12:09:44 -0500

    cleaned up test cases

--- rspec/board_rspec.rb
+++ rspec/board_rspec.rb
@@ -31,28 +31,23 @@ end
 RSpec.describe Board, "#validate" do
 	context "with valid move as argument" do
 		it "returns true if a move is invalid, false otherwise" do
-			move = 1
-			expect(board.validate(move)).to eq false
+			expect(board.validate(1)).to eq false
 		end
 	end
 	context "with array out of bounds move as argument" do
 		it "returns true if a move is invalid, false otherwise" do
-			move = -1
-			expect(board.validate(move)).to eq true
+			expect(board.validate(-1)).to eq true
 		end
 	end
 	context "with array out of bounds move as argument" do
 		it "returns true if a move is invalid, false otherwise" do
-			move = 9
-			expect(board.validate(move)).to eq true
+			expect(board.validate(9)).to eq true
 		end
 	end
 	context "with move that is already occupied as argument" do
 		it "returns true if a move is invalid, false otherwise" do
-			state = ["X", 2, 3, 4, 5, 6, 7, 8, 9]
-			board_occupied = Board.new state, turn
-			move = 0
-			expect(board_occupied.validate(move)).to eq true
+			board.state = ["X", 2, 3, 4, 5, 6, 7, 8, 9]
+			expect(board.validate(0)).to eq true
 		end
 	end
 end
@@ -60,25 +55,20 @@ end
 RSpec.describe Board, "#check_winner" do
 	context "with state where CPU is a winner" do
 		it "returns the winner if a winner is found, returns none otherwise" do
-			state = ["O", "O", \
-				"O", 4, 5, 6, 7, 8, 9]
-			board_cpu = Board.new state, turn
-			expect(board_cpu.check_winner).to eq "O"
+			board.state = ["O", "O", "O", 4, 5, 6, 7, 8, 9]
+			expect(board.check_winner).to eq "O"
 		end
 	end
 	context "with state where Human is a winner" do
 		it "returns the winner if a winner is found, returns none otherwise" do
-			state = ["X", "X", \
-				"X", 4, 5, 6, 7, 8, 9]
-			board_human = Board.new state, turn
-			expect(board_human.check_winner).to eq "X"
+			board.state = ["X", "X", "X", 4, 5, 6, 7, 8, 9]
+			expect(board.check_winner).to eq "X"
 		end
 	end
 	context "with state where the game is a tie" do
 		it "returns the winner if a winner is found, returns none otherwise" do
-			state = ["O", "O", "X", "X", 5, "X", "X", "O", "O"]
-			board_tie = Board.new state, turn
-			expect(board_tie.check_winner).to eq "none"
+			board.state = ["O", "O", "X", "X", 5, "X", "X", "O", "O"]
+			expect(board.check_winner).to eq "none"
 		end
 	end
 end
@@ -86,27 +76,25 @@ end
 RSpec.describe Board, "#game_over" do
 	context "with state where the game is a tie" do
 		it "returns true if the game is over, false otherwise" do
-			state = ["O", "O", "X", "X", "O", "X", "X", "O", "O"]
-			board_over = Board.new state, turn
-			expect(board_over.game_over).to eq true
+			board.state = ["O", "O", "X", "X", "O", "X", "X", "O", "O"]
+			expect(board.game_over).to eq true
 		end
 	end
 	context "with state where the human has won" do
 		it "returns true if the game is over, false otherwise" do
-			state = ["X", "X", "X", "O", "O", 6, 7, 8, 9]
-			board_over = Board.new state, turn
-			expect(board_over.game_over).to eq true
+			board.state = ["X", "X", "X", "O", "O", 6, 7, 8, 9]
+			expect(board.game_over).to eq true
 		end
 	end
 	context "with state where the cpu has won" do
 		it "returns true if the game is over, false otherwise" do
-			state = ["O", "O", "O", "X", "X", 6, 7, 8, 9]
-			board_over = Board.new state, turn
-			expect(board_over.game_over).to eq true
+			board.state = ["O", "O", "O", "X", "X", 6, 7, 8, 9]
+			expect(board.game_over).to eq true
 		end
 	end
 	context "with state where the game is not over" do
 		it "returns true if the game is over, false otherwise" do
+			board.state = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 			expect(board.game_over).to eq false
 		end
 	end
